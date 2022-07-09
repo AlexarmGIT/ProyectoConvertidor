87e1f8dd56a02f4acbb800f5c7b7818527fb2874
AlexarmGIT <105029785+AlexarmGIT@users.noreply.github.com>
2022-07-09 02:35:08 -0300

    incorporacion de Array

--- script.js
+++ script.js
@@ -14,15 +14,15 @@ function convertir() {
   }
    else if ( cripto === "eth"){
     (resultado = dolar/etereum)
-     alert("La cantidad de Etereum sera " + resultado)
+     alert("La cantidad de Etereum sera $" + resultado)
    }
      else if (cripto === "ada"){
       (resultado = dolar/ada)
-       alert("La cantidad de Ada sera " + resultado)
+       alert("La cantidad de Ada sera $" + resultado)
     }
       else if (cripto === "xrp"){
        (resultado = dolar/xrp)
-       alert("La cantidad de Xrp sera" + resultado)
+       alert("La cantidad de Xrp sera $" + resultado)
      }
 
  else {
@@ -30,6 +30,28 @@ function convertir() {
  }
 }
 
-
-
 convertir();
+
+const mostrarCripto=[];
+
+    let bitcoin = 19237;
+    let etereum = 1052;
+    let ada = 0.44;
+    let xrp = 0.31;
+
+ mostrarCripto.push({bitcoin:bitcoin, etereum:etereum, ada:ada, xrp:xrp}); 
+
+let pregunta = prompt("Le gustaria  conocer las  cotizaciones disponibles? Si/NO");
+
+if(pregunta ==="SI"|| "si"|| "Si"){
+  let carga = mostrarCripto.forEach(e => alert( "Las cotizaciones son : "+ "\n"+
+  "Bitcoin =$ "+e.bitcoin +"\n"+"Ethereum =$ "+ e.etereum +"\n"+"ADA =$ "+e.ada +"\n"+"Xrp =$ "+e.xrp));
+  console.log(carga);
+}
+  else if (pregunta ==="NO"|| "no"|| "No"){
+  alert("Muchas gracias por su consulta , vuelva pronto");
+  }
+else{
+    alert("datos incorrectos");
+    } 
+
